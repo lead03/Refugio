3a4559470c61b8a09087120c4fc646f73aa77a0b
Leandro Gastón Duhart <leandro.duhart@hotmail.com>
2023-04-03 03:17:09 -0300

    Styles

---
+++ Refugio/Controllers/LanguageController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Refugio.Controllers
+{
+    public class LanguageController : Controller
+    {
+        // GET: Language
+        public ActionResult ChangeLanguage(string language, string returnUrl)
+        {
+            // Establecer el idioma seleccionado en la cookie de la aplicación
+            HttpCookie cookie = new HttpCookie("language", language);
+            cookie.Expires = DateTime.Now.AddYears(1);
+            Response.Cookies.Add(cookie);
+            // Establecer el idioma actual de la aplicación
+            Session["Language"] = language;
+            // Redirigir al usuario a la página anterior después de cambiar el idioma
+            return Redirect(returnUrl);
+        }
+        // GET: Language/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Language/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Language/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Language/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Language/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Language/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Language/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
