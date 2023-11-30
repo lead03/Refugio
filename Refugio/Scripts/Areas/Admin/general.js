d92fc02ce6453b70faab452e69efe223750ef58e
Leandro Duhart <leandro.duhart@gmail.com>
2023-11-30 03:52:44 -0300

    Menú lateral andando

---
+++ Refugio/Scripts/Areas/Admin/general.js
@@ -0,0 +1,14 @@
+﻿{
+    let sideBar = document.querySelector('.side-bar');
+    let arrowCollapse = document.querySelector('#logo-name__icon');
+    sideBar.onclick = () => {
+        sideBar.classList.toggle('custom-collapse');
+        arrowCollapse.classList.toggle('custom-collapse');
+        if (arrowCollapse.classList.contains('custom-collapse')) {
+            arrowCollapse.classList =
+                'bx bx-arrow-from-left logo-name__icon custom-collapse';
+        } else {
+            arrowCollapse.classList = 'bx bx-arrow-from-right logo-name__icon';
+        }
+    };
+}
