0798540ef54aeaca7617fe17ff3a066acae1de68
Leandro Duhart <leandro.duhart@gmail.com>
2023-12-06 02:01:58 -0300

    Modal de delete en proceso

--- Refugio/Scripts/Areas/Admin/Views/VeterinarianList.js
+++ Refugio/Scripts/Areas/Admin/Views/VeterinarianList.js
@@ -3,6 +3,9 @@
     $(document).on('click', '.pager-index', loadPage);
     $('#btn-reset-filters').click(cleanFilters);
     $('.filter').change(setFilterIsModified);
+    $('.icon-button.delete').on('click', function (e) {
+        deleteFromList()
+    });
     submitForm();
 });
 
@@ -42,4 +45,27 @@ function submitForm() {
             alert(xhr + "status: " + status);
         }
     });
+}
+
+function deleteFromList() {
+    e.preventDefault();
+
+    // Obtén los datos de la fila actual
+    var firstName = $(this).closest('tr').find('td:eq(0)').text();
+    var lastName = $(this).closest('tr').find('td:eq(1)').text();
+    var speciality = $(this).closest('tr').find('td:eq(2)').text();
+    var professionalLicense = $(this).closest('tr').find('td:eq(3)').text();
+
+    // Actualiza el contenido del modal
+    $('#modal-delete .modal-title').html('@Refugio.Resources.Languages.Global.Delete @Refugio.Resources.Languages.Global.Veterinarian.ToLower()');
+    $('#modal-delete .modal-body p:eq(0)').html('@Refugio.Resources.Languages.Global.SureDelete @Refugio.Resources.Languages.Global.Veterinarian.ToLower()?');
+    $('#modal-delete .modal-body p:eq(1)').html('@Refugio.Resources.Languages.Global.Name: ' + lastName + ', ' + firstName + '<br />' +
+        '@Refugio.Resources.Languages.Global.Speciality: ' + speciality + '<br />' +
+        '@Refugio.Resources.Languages.Global.ProfessionalLicense: ' + professionalLicense);
+
+    // Actualiza el enlace de eliminación dentro del modal
+    $('#modal-delete .modal-footer a').attr('href', $(this).attr('href'));
+
+    // Muestra el modal
+    $('#modal-delete').modal('show');
 }
