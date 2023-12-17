ef4bfbedffefa3d5d0b0833ec63f5700d72ce897
Leandro Duhart <leandro.duhart@gmail.com>
2023-12-17 04:12:46 -0300

    Modal reestrucutacion parte 1 - funcionando

--- Refugio/Scripts/Areas/Admin/Views/VeterinarianList.js
+++ Refugio/Scripts/Areas/Admin/Views/VeterinarianList.js
@@ -52,74 +52,74 @@ function deleteFromList() {
     var professionalLicense = $(this).data('professional-licence');
     var veterinarianId = $(this).data('id');
     // Actualiza el contenido del modal
-    $('#generic-admin-modal .custom-modal-title-icon').addClass('fa fa-exclamation-triangle');
-    $('#generic-admin-modal .custom-modal-title').html(modalDeleteConfiguration.modalDeleteTitle);
-    $('#generic-admin-modal .custom-modal-body p:eq(0)').html(modalDeleteConfiguration.modalDeleteBodyAnswer);
-    $('#generic-admin-modal .custom-modal-body p:eq(1)').html(modalDeleteConfiguration.modalNameSection + ': '+ completeName + '<br />' +
+    $('#custom-modal .custom-modal-title-icon').addClass(modalDeleteConfiguration.modalIcon);
+    $('#custom-modal .custom-modal-title').html(modalDeleteConfiguration.modalDeleteTitle);
+    $('#custom-modal .custom-modal-body p:eq(0)').html(modalDeleteConfiguration.modalDeleteBodyAnswer);
+    $('#custom-modal .custom-modal-body p:eq(1)').html(modalDeleteConfiguration.modalNameSection + ': '+ completeName + '<br />' +
         modalDeleteConfiguration.modalSpecialitySection + ': ' + speciality + '<br />' +
         modalDeleteConfiguration.modalLicenceSection + ': ' + professionalLicense);
-    $('#generic-admin-modal .button-primary-text').html(modalDeleteConfiguration.modalPrimaryButtonText);
-    $('#generic-admin-modal .button-secondary-text').html(modalDeleteConfiguration.modalSecondaryButtonText);
+    $('#custom-modal .button-primary-text').html(modalDeleteConfiguration.modalPrimaryButtonText);
+    $('#custom-modal .button-secondary-text').html(modalDeleteConfiguration.modalSecondaryButtonText);
 
     if (modalDeleteConfiguration.modalType == 'danger') {
-        $('#generic-admin-modal .button-primary').removeClass('success');
-        $('#generic-admin-modal .button-primary').removeClass('warning');
-        $('#generic-admin-modal .button-primary').removeClass('info');
-        $('#generic-admin-modal .button-primary').addClass('danger');
-        $('#generic-admin-modal .custom-modal-header').removeClass('success');
-        $('#generic-admin-modal .custom-modal-header').removeClass('warning');
-        $('#generic-admin-modal .custom-modal-header').removeClass('info');
-        $('#generic-admin-modal .custom-modal-header').addClass('danger');
-        $('#generic-admin-modal .custom-modal-content').removeClass('success');
-        $('#generic-admin-modal .custom-modal-content').removeClass('warning');
-        $('#generic-admin-modal .custom-modal-content').removeClass('info');
-        $('#generic-admin-modal .custom-modal-content').addClass('danger');
+        $('#custom-modal .button-primary').removeClass('success');
+        $('#custom-modal .button-primary').removeClass('warning');
+        $('#custom-modal .button-primary').removeClass('info');
+        $('#custom-modal .button-primary').addClass('danger');
+        $('#custom-modal .custom-modal-header').removeClass('success');
+        $('#custom-modal .custom-modal-header').removeClass('warning');
+        $('#custom-modal .custom-modal-header').removeClass('info');
+        $('#custom-modal .custom-modal-header').addClass('danger');
+        $('#custom-modal .custom-modal-content').removeClass('success');
+        $('#custom-modal .custom-modal-content').removeClass('warning');
+        $('#custom-modal .custom-modal-content').removeClass('info');
+        $('#custom-modal .custom-modal-content').addClass('danger');
     }
     else if (modalDeleteConfiguration.modalType == 'success') {
-        $('#generic-admin-modal .button-primary').removeClass('danger');
-        $('#generic-admin-modal .button-primary').removeClass('warning');
-        $('#generic-admin-modal .button-primary').removeClass('info');
-        $('#generic-admin-modal .button-primary').addClass('success');
-        $('#generic-admin-modal .custom-modal-header').removeClass('danger');
-        $('#generic-admin-modal .custom-modal-header').removeClass('warning');
-        $('#generic-admin-modal .custom-modal-header').removeClass('info');
-        $('#generic-admin-modal .custom-modal-header').addClass('success');
-        $('#generic-admin-modal .custom-modal-content').removeClass('danger');
-        $('#generic-admin-modal .custom-modal-content').removeClass('warning');
-        $('#generic-admin-modal .custom-modal-content').removeClass('info');
-        $('#generic-admin-modal .custom-modal-content').addClass('success');
+        $('#custom-modal .button-primary').removeClass('danger');
+        $('#custom-modal .button-primary').removeClass('warning');
+        $('#custom-modal .button-primary').removeClass('info');
+        $('#custom-modal .button-primary').addClass('success');
+        $('#custom-modal .custom-modal-header').removeClass('danger');
+        $('#custom-modal .custom-modal-header').removeClass('warning');
+        $('#custom-modal .custom-modal-header').removeClass('info');
+        $('#custom-modal .custom-modal-header').addClass('success');
+        $('#custom-modal .custom-modal-content').removeClass('danger');
+        $('#custom-modal .custom-modal-content').removeClass('warning');
+        $('#custom-modal .custom-modal-content').removeClass('info');
+        $('#custom-modal .custom-modal-content').addClass('success');
     }
     else if (modalDeleteConfiguration.modalType == 'warning') {
-        $('#generic-admin-modal .button-primary').removeClass('danger');
-        $('#generic-admin-modal .button-primary').removeClass('success');
-        $('#generic-admin-modal .button-primary').removeClass('info');
-        $('#generic-admin-modal .button-primary').addClass('warning');
-        $('#generic-admin-modal .custom-modal-header').removeClass('danger');
-        $('#generic-admin-modal .custom-modal-header').removeClass('success');
-        $('#generic-admin-modal .custom-modal-header').removeClass('info');
-        $('#generic-admin-modal .custom-modal-header').addClass('warning');
-        $('#generic-admin-modal .custom-modal-content').removeClass('danger');
-        $('#generic-admin-modal .custom-modal-content').removeClass('success');
-        $('#generic-admin-modal .custom-modal-content').removeClass('info');
-        $('#generic-admin-modal .custom-modal-content').addClass('warning');
+        $('#custom-modal .button-primary').removeClass('danger');
+        $('#custom-modal .button-primary').removeClass('success');
+        $('#custom-modal .button-primary').removeClass('info');
+        $('#custom-modal .button-primary').addClass('warning');
+        $('#custom-modal .custom-modal-header').removeClass('danger');
+        $('#custom-modal .custom-modal-header').removeClass('success');
+        $('#custom-modal .custom-modal-header').removeClass('info');
+        $('#custom-modal .custom-modal-header').addClass('warning');
+        $('#custom-modal .custom-modal-content').removeClass('danger');
+        $('#custom-modal .custom-modal-content').removeClass('success');
+        $('#custom-modal .custom-modal-content').removeClass('info');
+        $('#custom-modal .custom-modal-content').addClass('warning');
     }
     else if (modalDeleteConfiguration.modalType == 'info') {
-        $('#generic-admin-modal .button-primary').removeClass('danger');
-        $('#generic-admin-modal .button-primary').removeClass('success');
-        $('#generic-admin-modal .button-primary').removeClass('warning');
-        $('#generic-admin-modal .button-primary').addClass('info');
-        $('#generic-admin-modal .custom-modal-header').removeClass('danger');
-        $('#generic-admin-modal .custom-modal-header').removeClass('success');
-        $('#generic-admin-modal .custom-modal-header').removeClass('warning');
-        $('#generic-admin-modal .custom-modal-header').addClass('info');
-        $('#generic-admin-modal .custom-modal-content').removeClass('danger');
-        $('#generic-admin-modal .custom-modal-content').removeClass('success');
-        $('#generic-admin-modal .custom-modal-content').removeClass('warning');
-        $('#generic-admin-modal .custom-modal-content').addClass('info');
+        $('#custom-modal .button-primary').removeClass('danger');
+        $('#custom-modal .button-primary').removeClass('success');
+        $('#custom-modal .button-primary').removeClass('warning');
+        $('#custom-modal .button-primary').addClass('info');
+        $('#custom-modal .custom-modal-header').removeClass('danger');
+        $('#custom-modal .custom-modal-header').removeClass('success');
+        $('#custom-modal .custom-modal-header').removeClass('warning');
+        $('#custom-modal .custom-modal-header').addClass('info');
+        $('#custom-modal .custom-modal-content').removeClass('danger');
+        $('#custom-modal .custom-modal-content').removeClass('success');
+        $('#custom-modal .custom-modal-content').removeClass('warning');
+        $('#custom-modal .custom-modal-content').addClass('info');
     }
     // Actualiza el enlace de eliminación dentro del modal
-    $('#generic-admin-modal .button-primary').attr('href', modalDeleteConfiguration.modalUrlAction + veterinarianId);
+    $('#custom-modal .button-primary').attr('href', modalDeleteConfiguration.modalUrlAction + veterinarianId);
 
     // Muestra el modal
-    $('#generic-admin-modal').modal('show');
+    $('#custom-modal').modal('show');
 }
