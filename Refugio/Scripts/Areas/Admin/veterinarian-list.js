c0991a3cbb35574532c25121cd6a60d85a4e4011
Leandro Duhart <leandro.duhart@gmail.com>
2024-05-06 07:54:01 -0300

    Mejoras generales

--- Refugio/Scripts/Areas/Admin/veterinarian-list.js
+++ Refugio/Scripts/Areas/Admin/veterinarian-list.js
@@ -1,48 +1,9 @@
 ﻿$(document).ready(function () {
-    $('#btn-search').click(loadFilters);
-    $(document).on('click', '.pager-index', loadPage);
-    $('#btn-reset-filters').click(cleanFilters);
-    //$('.filter').change(setFilterIsModified);
     $(document).on('click', '.delete', deleteFromList);
-    submitForm();
 });
 
-function loadFilters() {
-    event.preventDefault();
-    $('#hdn-pager-index').val(1);
-    submitForm();
-}
-
-function loadPage() {
-    event.preventDefault();
-    var targetPage = parseInt($(this).data('page-index'));
-    $('#hdn-pager-index').val(targetPage);
-    submitForm();
-}
-
-//function setFilterIsModified() {
-//    $('#hdn-filter-modified-indicator').val(true);
-//}
-
-function cleanFilters() {
-    event.preventDefault();
-    $('#form-veterinarian-list').trigger("reset");
-    $('#hdn-pager-index').val(1);
-    //$('#hdn-filter-modified-indicator').val(true);
-    submitForm();
-}
-
 function submitForm() {
-    $.ajax({
-        url: "/Veterinarian/VeterinarianList",
-        data: $("#form-veterinarian-list").serialize(),
-        success: function (result) {
-            $("#veterinarian-list-container").html(result);
-        },
-        error: function (xhr, status, error) {
-            alert(xhr + "status: " + status);
-        }
-    });
+    submitGenericForm("/Veterinarian/VeterinarianList", "#form-veterinarian-list", "#veterinarian-list-container");
 }
 
 function deleteFromList() {
---
+++ Refugio/Scripts/Shared/form-pager.js
@@ -0,0 +1,43 @@
+﻿$(document).ready(function () {
+    $('#btn-search').click(loadFilters);
+    $(document).on('click', '.pager-index', loadPage);
+    $('#btn-reset-filters').click(function (event) {
+        cleanFilters(event, this);
+    });
+    submitForm();
+});
+
+function loadFilters(event) {
+    event.preventDefault();
+    $('#hdn-pager-index').val(1);
+    submitForm();
+}
+
+function loadPage(event) {
+    event.preventDefault();
+    var targetPage = parseInt($(this).data('page-index'));
+    $('#hdn-pager-index').val(targetPage);
+    submitForm();
+}
+
+function cleanFilters(event, button) {
+    event.preventDefault();
+    var form = $(button).closest('form');
+    form.trigger("reset");
+    form.find('#hdn-pager-index').val(1);
+    submitForm();
+}
+
+function submitGenericForm(url, formDataSelector, resultContainerSelector) {
+    var formData = $(formDataSelector).serialize();
+    $.ajax({
+        url: url,
+        data: formData,
+        success: function (result) {
+            $(resultContainerSelector).html(result);
+        },
+        error: function (xhr, status, error) {
+            alert(xhr + "status: " + status);
+        }
+    });
+}
