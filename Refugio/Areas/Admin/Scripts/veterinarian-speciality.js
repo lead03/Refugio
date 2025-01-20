33eb9d0c2d4e65a2de2ff5c3b1bde7484912e482
Leandro Duhart <leandro.duhart@gmail.com>
2025-01-20 04:47:01 -0300

    #86b3hx9fu - Actualizaciones y agergado de validaciones Unobstrusive funcionando correctamente.

---
+++ Refugio/Areas/Admin/Scripts/veterinarian-speciality.js
@@ -0,0 +1,24 @@
+﻿$(document).ready(function () {
+    $("#veterinarianSpecialityForm").validate({
+        rules: {
+            SpecialityNameES: {
+                required: true,
+                maxlength: 100
+            },
+            SpecialityNameEN: {
+                required: true,
+                maxlength: 100
+            }
+        },
+        messages: {
+            SpecialityNameES: {
+                required: "Este campo es obligatorio.",
+                maxlength: "No puede superar los 100 caracteres."
+            },
+            SpecialityNameEN: {
+                required: "Este campo es obligatorio.",
+                maxlength: "No puede superar los 100 caracteres."
+            }
+        }
+    });
+});
